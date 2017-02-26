0a9407869f119acdda65a25ba62d283be7ac703a
Michael Chavez <bigbassroller@gmail.com>
2017-02-26 13:25:30 -0800

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PostsComponent } from './posts/posts.component';
 import { PostsEditComponent } from './posts-edit/posts-edit.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { PublicComponent } from './public/public.component';
+import { PrivateComponent } from './private/private.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
     PostsEditComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    PublicComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
