2bd0857c99e529270f3f5f8637fec443298d3fe5
Michael Chavez <bigbassroller@gmail.com>
2017-02-26 13:07:25 -0800

    added private and public to module

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from '../app.component';
 import { PostsComponent } from '../posts/posts.component';
 import { PostsEditComponent } from '../posts-edit/posts-edit.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
+import { PublicComponent } from '../public/public.component';
+import { PrivateComponent } from '../private/private.component';
 
 const appRoutes: Routes = [
 	{ path: 'posts', component: PostsComponent },
 	{ path: 'posts-edit', component: PostsEditComponent },
-	{ path: 'not-found', component: NotFoundComponent }
+	{ path: 'not-found', component: NotFoundComponent },
+  { path: 'public', component: PublicComponent },
+  { path: 'private', component: PrivateComponent }
 ]
 
 
